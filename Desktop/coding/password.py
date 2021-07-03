958ce3aadc51d1bc0fbe65b84b6b653259add9f0
stanly <dk3vup12018@gmail.com>
2021-07-03 15:12:00 +0800

    basic version done

---
+++ Desktop/coding/password.py
@@ -0,0 +1,12 @@
+passward = 'a123456'
+i = 3 
+while True:
+	time = input('請輸入密碼:')
+	if time == passward:
+		print('登入成功')
+		break
+	else:
+		i = i - 1
+		print('密碼錯誤!還有', i, '次機會' )
+		if i == 0:
+			break
